ba9fa1d68bb7e1c9b823c76337e271ea45fb82ae
Kenan Berber <kenanbrbr@gmail.com>
2023-11-10 11:28:44 +0300

    All pages design's have been created

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Kenan Berber",
+  description: "Kenan Berber Personal Website!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b className={styles.bold}>Hey, Kenan Berber here!</b> sitamet
+        consectetur adipisicing elit.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.{" "}
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            quisquam facilis fugiat optio id odio, beatae fugit incidunt ut
+            ullam mollitia minus laboriosam itaque voluptates quia dignissimos
+            magni? Maiores, in.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
